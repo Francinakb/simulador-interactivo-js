ad5a63586a768abe00c802dd27ac9eaafccf8405
Francina Martín <francinakb@gmail.com>
2022-07-13 17:10:09 -0300

    re entrega desafio 2

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 // La idea de este simulador es dar un estimativo de promedio de edades de alumnxs para saber a que curso estimativo pertenecen
 
+alert("Bienvenido! Se han registrado un total de 6 alumnxs nuevos. Por favor, ingrese la edad de cada unx para entregarles el promedio equivalente de sus edades.")
+
+//aplico una funcion flecha para el registro de dichos alumnxs donde aclaro la suma de sus edades, y que retorne el promedio
+const promedio = (edad1, edad2, edad3, edad4, edad5, edad6) => (edad1 + edad2 + edad3 + edad4 + edad5 + edad6)/6
 
 //aplico las variables para 6 alumnxs y sus respectivas edades
-let edad1 = parseFloat(prompt("Han registrado un total de 6 alumnxs nuevos. Por favor, ingrese la edad de cada unx para entregarles el promedio equivalente de sus edades. Edad alumnx 1:"))
-let edad2 = parseFloat(prompt("Edad de alumnx 2:"))
-let edad3 = parseFloat(prompt("Edad de alumnx 3:"))
-let edad4 = parseFloat(prompt("Edad de alumnx 4:"))
-let edad5 = parseFloat(prompt("Edad de alumnx 5:"))
-let edad6 = parseFloat(prompt("Edad de alumnx 6:"))
+let edad1, edad2, edad3, edad4, edad5, edad6
 
-//aplico una funcion para el registro de dichos alumnxs donde aclaro la suma de sus edades, y que retorne el promedio
-function registroAlumnxs(edad1, edad2, edad3, edad4, edad5, edad6) {
+//aplico ciclo do while 
+do{
+    edad1 = parseInt(prompt("Ingrese edad del alumnx 1:"))
+    edad2 = parseInt(prompt("Ingrese edad del alumnx 2:")) 
+    edad3 = parseInt(prompt("Ingrese edad del alumnx 3:"))
+    edad4 = parseInt(prompt("Ingrese edad del alumnx 4:"))
+    edad5 = parseInt(prompt("Ingrese edad del alumnx 5:"))
+    edad6 = parseInt(prompt("Ingrese edad del alumnx 6:"))
 
-    let edades = edad1 + edad2 + edad3 + edad4 + edad5 + edad6
+    //aplico algoritmo condicional
+    if(isNaN(edad1) || isNaN(edad2) || isNaN(edad3) || isNaN(edad4) || isNaN(edad5) || isNaN(edad6)){
+        alert ("Ingrese numeros enteros")
+    }
+    
+    // alert y concateno con backticks el promedio 
+    alert(`El promedio de edad de lxs alumnxs registradxs es de ${promedio(edad1, edad2, edad3, edad4, edad5, edad6)} años.`)
 
-    let promedio = edades / 6
-
-    return promedio
+}while(isNaN(edad1) || isNaN(edad2) || isNaN(edad3) || isNaN(edad4) || isNaN(edad5) || isNaN(edad6))
 
 
-}    if(isNaN(edad1) || isNaN(edad2) || isNaN(edad3) || isNaN(edad4) || isNaN(edad5) || isNaN(edad6)){
-    alert ("No está ingresando una edad correcta, intente nuevamente")
-}
 
-// console.log concateno con backticks el promedio y el registro
-console.log (`El promedio de edad de lxs alumnxs registradxs es de ${registroAlumnxs(edad1, edad2, edad3, edad4, edad5, edad6)} años.`)
+
+
+
 
 
 
